c734d170f5134d97ce1ec6949108b275c18c14d5
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2026-02-17 10:00:37 -0500

    Adición metodo Sumar

--- src/main/java/co/vinni/Main.java
+++ src/main/java/co/vinni/Main.java
@@ -1,10 +1,16 @@
 package co.vinni;
 
+import co.vinni.util.Operaciones;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Proyecto base tipo Maven-Java");
+
+        Operaciones op = new Operaciones();
+        long resultado = op.sumar(2,2);
+        System.out.println(resultado);
 
     }
 }
---
+++ src/main/java/co/vinni/util/Operaciones.java
@@ -0,0 +1,8 @@
+package co.vinni.util;
+
+public class Operaciones {
+
+    public long sumar (int a, int b){
+        return a+b;
+    }
+}
